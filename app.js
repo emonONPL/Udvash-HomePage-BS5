af8f26b38c7d9d2f1e13f88245ead372ad033531
Emon <emon.onnorokom@gmail.com>
2025-06-18 16:15:15 +0600

    Completed Carousel

---
+++ app.js
@@ -0,0 +1,8 @@
+// ----- Navbar -----
+
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
+    $(".navbar-toggler-icon").toggleClass("d-none");
+    $(".fa-xmark").toggleClass("d-none");
+  });
+});
